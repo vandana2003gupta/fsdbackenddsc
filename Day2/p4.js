56e4d7221636263fd8e0ae070b5306f73f69fabf
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-02-24 15:19:26 +0530

    http sever json

---
+++ Day2/p4.js
@@ -0,0 +1,24 @@
+const http= require('http');
+const server=http.createServer((req,res)=>{
+    let data=[
+        {
+            "id": 1,
+            "username": "admin",
+            "password": "1234"
+        },
+        {
+            "id": 2,
+            "username": "user",
+            "password": "5678"
+        }
+    ];
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.end(JSON.stringify(data));
+});
+
+server.listen(9000,(err)=>{
+    if(err)
+        console.log("Error starting server",err)
+    else
+        console.log("Server started on port 9000")
+ });
